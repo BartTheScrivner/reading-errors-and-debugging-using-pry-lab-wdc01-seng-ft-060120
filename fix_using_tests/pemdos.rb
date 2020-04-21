8b09ffc90327a9fbc75b303f2c00e5163988e109
fantastic-scope-1514 <dframe22@gmail.com>
2020-04-21 20:47:52 +0000

    Automatically backed up by Learn

--- fix_using_tests/pemdos.rb
+++ fix_using_tests/pemdos.rb
@@ -2,8 +2,13 @@
 require 'pry'
 def snake_it_up(string)
   if string[0] == "s"
-    new = 10 * "s" + string
+    new = ""
+    count = 0
+    while count < 10 do
+      new += "s"
+      count += 1
   else
+    new += string
     new
   end
 end
